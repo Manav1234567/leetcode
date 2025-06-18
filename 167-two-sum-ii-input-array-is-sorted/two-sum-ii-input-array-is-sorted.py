1b61517f69ce9075dfafd28073aa080728e2c850
Manav1234567 <89704952+Manav1234567@users.noreply.github.com>
2025-06-18 19:02:06 +0530

    Time: 3 ms (81.19%) | Memory: 18.5 MB (59.43%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -3,12 +3,9 @@ class Solution:
         hashmap = {}
         for i in range(len(numbers)):
             hashmap[numbers[i]] = i
-        print(hashmap)
-        print(range(len(numbers)), len(numbers), numbers)
         for i in range(len(numbers)):
-            print(i)
             complement = target - numbers[i]
             if complement in hashmap and hashmap[complement] != i:
                 return [i+1, hashmap[complement]+1]
-                
+
         return []
