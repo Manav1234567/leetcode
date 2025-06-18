4540f1b477041b0b05e306c83c954572dc42876c
Manav1234567 <89704952+Manav1234567@users.noreply.github.com>
2025-06-18 19:01:23 +0530

    Time: 9 ms (35.42%) | Memory: 19.1 MB (24.11%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        print(hashmap)
+        print(range(len(nums)), len(nums), nums)
+        for i in range(len(nums)):
+            print(i)
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
+        # If no valid pair is found, return an empty list
+        return []
