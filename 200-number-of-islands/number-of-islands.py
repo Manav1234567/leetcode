843c7a13031a3aa1273f42c4ca04af752816c4d8
Manav1234567 <89704952+Manav1234567@users.noreply.github.com>
2026-02-18 01:25:42 +1100

    Time: 228 ms (92.30%) | Memory: 21.7 MB (63.69%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,28 +3,27 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
 
-        visited = [[0 for _ in range(cols)] for _ in range(rows)]
         counter = 0
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(rows):
+            for j in range(cols):
                 stack = []
 
-                if (grid[i][j] == "1" and visited[i][j] == 0):
+                if grid[i][j] == "1":
                     counter += 1
                     stack.append((i, j))
 
-                while stack != []:
+                while stack:
                     a, b = stack.pop()
-                    visited[a][b] = 1
+                    grid[a][b] = "0"
 
-                    if a >= 1 and grid[a - 1][b] == "1" and visited[a - 1][b] == 0: 
+                    if a >= 1 and grid[a - 1][b] == "1": 
                         stack.append((a - 1, b))
-                    if b >= 1 and grid[a][b - 1] == "1" and visited[a][b - 1] == 0: 
+                    if b >= 1 and grid[a][b - 1] == "1": 
                         stack.append((a, b - 1))
-                    if a < rows - 1 and grid[a + 1][b] == "1" and visited[a + 1][b] == 0: 
+                    if a < rows - 1 and grid[a + 1][b] == "1": 
                         stack.append((a + 1, b))
-                    if b < cols - 1 and grid[a][b + 1] == "1" and visited[a][b + 1] == 0: 
+                    if b < cols - 1 and grid[a][b + 1] == "1": 
                         stack.append((a, b + 1))
 
         return counter
