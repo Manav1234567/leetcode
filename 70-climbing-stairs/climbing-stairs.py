9ee95a6bd12387b51f6137c03755b85994b96e52
Manav1234567 <89704952+Manav1234567@users.noreply.github.com>
2026-02-18 16:41:48 +1100

    Time: 0 ms (100.00%) | Memory: 19.5 MB (13.57%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,14 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
+        if n <= 2: return n
         
         n_2 = 2
         n_1 = 1
 
         for i in range(3, n + 1):
-            temp = n_2 + n_1
-            n_1 = n_2
-            n_2 = temp
+            n_1, n_2 = n_2, n_1 + n_2
         
         return n_2
