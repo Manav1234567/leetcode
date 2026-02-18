3660e8b3dcf11040c22daf0c56b624c2a931ac68
Manav1234567 <89704952+Manav1234567@users.noreply.github.com>
2026-02-18 16:38:04 +1100

    Time: 0 ms (100.00%) | Memory: 19.2 MB (71.55%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,14 +1,15 @@
 class Solution:
-    def __init__ (self):
-        self.hmap = {}
-
     def climbStairs(self, n: int) -> int:
-        if n in self.hmap: 
-            return self.hmap[n]
-        if n <= 2: 
-            self.hmap[n] = n
+        if n <= 2:
             return n
         
-        self.hmap[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        n_2 = 2
+        n_1 = 1
+        ans = n_2 + n_1
 
-        return self.hmap[n]
+        for i in range(3, n + 1):
+            ans = n_2 + n_1
+            n_1 = n_2
+            n_2 = ans
+        
+        return ans
