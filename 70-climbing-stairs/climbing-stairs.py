32039c9df1f6debcebd3d9ccdbcee03b56177fc6
Manav1234567 <89704952+Manav1234567@users.noreply.github.com>
2026-02-18 16:13:57 +1100

    Time: 0 ms (100.00%) | Memory: 19.4 MB (13.57%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -5,12 +5,10 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n in self.hmap: 
             return self.hmap[n]
-        elif n <= 2: 
+        if n <= 2: 
             self.hmap[n] = n
             return n
         
-        left = self.hmap[n-1] if n-1 in self.hmap else self.climbStairs(n - 1)
-        right = self.hmap[n-2] if n-2 in self.hmap else self.climbStairs(n - 2)
-        self.hmap[n] = left + right
+        self.hmap[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
 
         return self.hmap[n]
